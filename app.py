6b0acbcda1939c9215ceed44c2ae7ceda380a8c7
fabiano <fsousapaixao@hotmail.com>
2024-03-16 22:13:48 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+import openpyxl
+from PIL import Image, ImageDraw, ImageFont
+
+
+#Abrindo a planilha
+workbook_alunos = openpyxl.load_workbook('planilha_alunos.xlsx')
+sheet_alunos = workbook_alunos['Sheet1']
+
+
+for indice, linha in enumerate(sheet_alunos.iter_rows(min_row=2)): #emite apenas um certificado (min_row=2,max_row=2)
+    #Cada célula contém a informação que precisamos
+    nome_curso = linha[0].value #nome do curso
+    nome_participante = linha[1].value #nome do participante
+    tipo_participacao = linha[2].value #tipo de participação
+    data_inicio = linha[3].value # data de inicio
+    data_final = linha[4].value # data de termíno
+    carga_horaria = linha[5].value #carga horaria
+    data_emissao = linha[6].value # data de emissão
+   
+   # Transferir os dados para a imagem do certificado
+    #Definindo a fonte a ser usada
+    fonte_nome = ImageFont.truetype('./tahomabd.ttf', 90)
+    fonte_geral = ImageFont.truetype('./tahoma.ttf', 80)
+    fonte_data = ImageFont.truetype('./tahoma.ttf', 55)
+
+    image =Image.open("./certificado_padrao.jpg")
+    desenhar = ImageDraw.Draw(image)
+
+    #parte principal
+    desenhar.text((1020,827), nome_participante,fill='black', font=fonte_nome)
+    desenhar.text((1069,950), nome_curso,fill='black', font=fonte_geral)
+    desenhar.text((1435,1065), tipo_participacao,fill='black', font=fonte_geral)
+    desenhar.text((1489,1182), str(carga_horaria),fill='black', font=fonte_geral)
+
+    #parte das datas (inicio e termino)
+    desenhar.text((750,1770), data_inicio,fill='blue', font=fonte_data)
+    desenhar.text((750,1930), data_final,fill='blue', font=fonte_data)
+
+    #parte da data de emisssão
+    desenhar.text((2220,1930), data_emissao,fill='blue', font=fonte_data)
+
+
+    image.save(f'./{indice}{nome_participante}certificado.png')
+
+
